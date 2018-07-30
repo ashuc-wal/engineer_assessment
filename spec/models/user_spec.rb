ba41484cfbc84da576d8e099cd95049ef106c639
Ashu Chauhan <ashu.c@westagilelabs.com>
2018-07-30 17:26:52 +0530

    model test case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#generate_token" do
+    example "should encode payload and expire time should 1 hour" do
+      user = create(:user)
+      current_time = Time.current
+      allow(Time).to receive(:current).and_return(current_time)
+      token = user.generate_token
+      payload = JWT.decode(token,
+         Rails.application.credentials[:secret_key_base].to_s,
+         true, { algorithm: User::ALGORITHM}).first
+
+      expect(payload["exp"]).to eql(current_time.to_i + 3600)
+      expect(payload["email"]).to eql(user.email)
+    end
+  end
 end
